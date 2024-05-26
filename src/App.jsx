407e1097b9786d3b0f47995641043337c3b740b6
JesusVA760 <tonio4340406@gmail.com>
2024-05-25 22:41:58 -0700

    NextStep

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Personal from "./Pages/Personal";
 import Select from "./Pages/Select";
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Personal} />
-          <Route path="/select" component={Select} />
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Personal />} />
+          <Route path="/select" element={<Select />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
