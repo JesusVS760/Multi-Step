a099e205afea2d470cce3525bbf8bc135e2d708f
JesusVA760 <tonio4340406@gmail.com>
2024-05-28 13:21:44 -0700

    add-ons

--- src/Pages/AddOns.jsx
+++ src/Pages/AddOns.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import Steps from "../components/Steps";
+import PrevStep from "../components/PrevStep";
+import NextStep from "../components/NextStep";
+import "./AddOns.css";
 
 const AddOns = () => {
-  return <div>AddOns</div>;
+  return (
+    <div className="add-ons-container">
+      <div className="sidebar-container">
+        <Steps page={3} />
+      </div>
+      <div className="add-ons-content">
+        <div className="add-ons-header">
+          <h1>Pick add-ons</h1>
+          <p>Add-ons help enhance your gaming experience</p>
+        </div>
+        <div className="add-on-options">
+          <div className="add-on-option">
+            <button>
+              <input type="checkbox" />
+              <div className="add-on-text">
+                <h3>Online service</h3>
+                <p>Access to multiplayer games</p>
+              </div>
+            </button>
+          </div>
+          <div className="add-on-option">
+            <button>
+              <input type="checkbox" />
+              <div className="add-on-text">
+                <h3>Larger storage</h3>
+                <p>Extra 1TB of cloud save</p>
+              </div>
+            </button>
+          </div>
+          <div className="add-on-option">
+            <button>
+              <input type="checkbox" />
+              <div className="add-on-text">
+                <h3>Customizable Profile</h3>
+                <p>Custom theme on your profile</p>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="next-step-button">
+        <PrevStep to="/select" />
+        <NextStep to="/summary" />
+      </div>
+    </div>
+  );
 };
 
 export default AddOns;
