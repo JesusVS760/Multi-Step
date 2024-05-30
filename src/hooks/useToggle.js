4309d3ff5449115c3377eccb7092ccadbf484b4c
JesusVA760 <tonio4340406@gmail.com>
2024-05-30 16:08:05 -0700

    useToggle

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,25 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useToggle = () => {
-  const [isClicked, setIsClicked] = useState({
+  const initialState = JSON.parse(localStorage.getItem("toggleState")) || {
     arcadeOpt: false,
     advancedOpt: false,
     proOpt: false,
     onlineService: false,
     LargerStorage: false,
     customizableProfile: false,
-  });
+  };
+  const [isClicked, setIsClicked] = useState(initialState);
 
   const handleClick = (button) => {
     setIsClicked((prevState) => ({
-      ...Object.fromEntries(
-        Object.entries(prevState).map(([key, value]) => [
-          key,
-          key === button ? !value : false,
-        ])
-      ),
+      ...prevState,
+      [button]: !prevState[button],
     }));
   };
+  //saving to localStorage in order to access values later on
+  useEffect(() => {
+    localStorage.setItem("toggleState", JSON.stringify(isClicked));
+  }, [isClicked]);
+
   return { isClicked, handleClick };
 };
 export default useToggle;
